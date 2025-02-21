cc157617b28afbad986c067e8d0d45a61186bdd3
Matija Blagojevic <matijamaxi@gmail.com>
2025-02-21 18:13:23 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,61 @@
-import { Metadata } from 'next/server';
+export const metadata = {
 
-export const metadata: Metadata = {
-  title: 'Euroleague guessing game',
-  description: 'Created by Matija Blagojevic',
-  icons: {  // Add the icons property
-    icon: '/EL_E-Ball_Orange_PMS.png', // Path to your favicon.ico in the public directory
-    
-  },
-};
+  title: 'Euroleague guessing game',
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+  description: 'Created by Matija Blagojevic',
+
+}
+
+
+
+export default function RootLayout({
+
+  children,
+
+}: {
+
+  children: React.ReactNode
+
+}) {
+
+  return (
+
+    <html lang="en">
+
+      <body>{children}</body>
+
+    </html>
+
+  )
+
+}export const metadata = {
+
+  title: 'Euroleague guessing game',
+
+  description: 'Created by Matija Blagojevic',
+
+}
+
+
+
+export default function RootLayout({
+
+  children,
+
+}: {
+
+  children: React.ReactNode
+
+}) {
+
+  return (
+
+    <html lang="en">
+
+      <body>{children}</body>
+
+    </html>
+
+  )
+
 }
