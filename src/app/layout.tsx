0f247041cdb91b5f160fcc4595b567e3d8e6ed72
Matija Blagojevic <matijamaxi@gmail.com>
2025-02-21 17:48:20 +0100

    Update layout.tsx
    Update the icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,18 @@
-export const metadata = {
+import { Metadata } from 'next/server';
+
+export const metadata: Metadata = {
   title: 'Euroleague guessing game',
   description: 'Created by Matija Blagojevic',
-}
+  icons: {  // Add the icons property
+    icon: '/EL_E-Ball_Orange_PMS.png', // Path to your favicon.ico in the public directory
+    
+  },
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
