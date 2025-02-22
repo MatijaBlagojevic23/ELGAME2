41a1ef2a6e6bbed9bc10dc09090332657fc3f8c3
Matija Blagojevic <matijamaxi@gmail.com>
2025-02-22 12:47:04 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
   title: 'Euroleague Guessing Game',
-  description: 'Created by Matjia Blagojevic',
+  description: 'Created by Matija Blagojevic',
 }
 
 export default function RootLayout({
@@ -10,7 +10,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        {/* You can add other meta tags or links here */}
+      </head>
       <body>{children}</body>
     </html>
   )
 }
+
