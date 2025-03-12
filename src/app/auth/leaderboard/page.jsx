fde3146b1fc00cfc6e4eb5dbdef729b8b36d88f9
Matija Blagojevic <matijamaxi@gmail.com>
2025-03-12 10:01:05 +0100

    Update page.jsx

--- src/app/auth/leaderboard/page.jsx
+++ src/app/auth/leaderboard/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { supabase } from "../../utils/supabase";
+import { supabase } from "../../../utils/supabase";
 import Link from "next/link";
 
 export default function Leaderboard() {
