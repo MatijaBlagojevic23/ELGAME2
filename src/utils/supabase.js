0f2809a0edbe9c2aae22ef078b99a4b2dd4370c2
Matija Blagojevic <matijamaxi@gmail.com>
2025-03-13 09:57:48 +0100

    Update supabase.js

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -3,4 +3,14 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: {
+    persistSession: true, // Enable session persistence
+    storageType: "cookie", // Tell Supabase to store tokens in cookies
+    cookieOptions: {
+      secure: process.env.NODE_ENV === "production", // Set "secure" in production
+      // Add "sameSite" attribute here if needed (e.g., 'lax' or 'strict')
+      // sameSite: 'lax',
+    },
+  },
+});
