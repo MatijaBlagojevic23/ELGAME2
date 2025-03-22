42cd9940ea0fb2a1e98c2a841366b295d4c5ad9b
Matija Blagojevic <matijamaxi@gmail.com>
2025-03-22 13:29:42 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 import ELGAME from "../components/ELGAME";
 
 export default async function Page() {
