e41d2ae13b8b0f1d07494814526b493c3e677746
Matija Blagojevic <matijamaxi@gmail.com>
2025-03-23 19:57:13 +0100

    Update route.js

--- src/app/api/check-game-state/route.js
+++ src/app/api/check-game-state/route.js
@@ -1,4 +1,4 @@
-import { supabase } from "../../../../utils/supabase";
+import { supabase } from "../../../utils/supabase";
 
 export async function GET(request) {
   const userId = request.nextUrl.searchParams.get("userId");
