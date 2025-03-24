67d047acadd19f864ad9ef8cac5dc994bcaccb3f
Matija Blagojevic <matijamaxi@gmail.com>
2025-03-24 13:25:04 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -510,7 +510,8 @@ export default function ELGAME() {
           </div>
         </div>
       )}
-
+  {showLogout && (
+  
       <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
           <div className="bg-white p-6 rounded-md shadow-lg text-center">
             <p className="text-lg font-bold mb-4">Are you sure you want to log out? You will lose your data.</p>
