40820e9a2f636087b7fc14f46825a12e1977d452
Matija Blagojevic <matijamaxi@gmail.com>
2025-03-24 20:24:23 +0100

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -491,7 +491,7 @@ export default function ELGAME() {
       {showPlayedPopup && (
         <div className="fixed inset-0 flex flex-col items-center justify-center bg-black bg-opacity-50 z-50 p-4">
           <div className="bg-white p-6 rounded-md shadow-lg text-center">
-            <p className="text-lg font-bold mb-4">You have already played today. The correct player was {target?.name}</p>
+            <p className="text-lg font-bold mb-4">You have already played today.</p>
             <button
               onClick={() => setShowPlayedPopup(false)}
               className="bg-gray-500 text-white px-4 py-2 rounded-md shadow-md transition-transform hover:scale-105 hover:bg-gray-600"
