72a3be7050accf175314b9abab4413da91c467ee
Matija Blagojevic <matijamaxi@gmail.com>
2025-03-29 14:59:36 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    appDir: true,
+  },
+  /* other config options here */
 };
 
 export default nextConfig;
